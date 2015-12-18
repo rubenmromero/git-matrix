8c5997a49e40ab53a4c3065ab171600dfa8e529d
Ruben Martin <rubenmromero@gmail.com>
2015-12-18 14:26:34 +0100

    Configure the branches to use

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 mkdir -p ./bin
-curl --output ./bin/git_matrix.sh https://raw.githubusercontent.com/rubenmromero/git-matrix/master/git_matrix.sh
+echo -e "\nSpecify the environment branches to manage in your project: \c"
+read BRANCHES
+curl --silent https://raw.githubusercontent.com/rubenmromero/git-matrix/master/git_matrix.sh |sed "s/ENV_BRANCHES=(.*)/ENV_BRANCHES=($BRANCHES)/g" >./bin/git_matrix.sh
 chmod 755 ./bin/git_matrix.sh
